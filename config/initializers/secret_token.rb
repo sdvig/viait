98727f09f21fdde7566e405aa33e4cbfc5da6ebb
Sofiya Tepikin <me@kvazar.co.il>
2013-10-13 11:38:11 +0200

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Viait::Application.config.secret_token = 'c5cf77f51b9baf08c69341d575daf1c748b4dab16b5805c0c4a7e830eeabedbed47bbc6f4fb998fd6ac38ab9b61ea28ca789bf8bd5829531ed568fffef9942f4'
